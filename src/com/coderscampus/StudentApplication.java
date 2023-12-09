667656cc2b603c198b781b24dd143815f1ddf8b9
Charles Knowlton <knowlton2023@users.noreply.github.com>
2023-12-09 08:02:52 -0600

    WIP

--- src/com/coderscampus/StudentApplication.java
+++ src/com/coderscampus/StudentApplication.java
@@ -1,10 +1,12 @@
 package com.coderscampus;
 
+import java.io.IOException;
+
 public class StudentApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		StudentService student = new StudentService();
-
+		System.out.println(student.sortCourse1());
 
 	}
 
